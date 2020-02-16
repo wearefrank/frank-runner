6df9656f674b36a246c70d5541a5c14a250a5def
Jaco de Groot <jaco.de.groot@ibissource.org>
2020-02-16 15:42:19 +0100

    Fix passing arguments like -Dproject.dir

--- run.bat
+++ run.bat
@@ -42,7 +42,7 @@ if not exist "%~dp0build\apache-ant-1.10.7\" (
 	)
 )
 set JAVA_HOME=%~dp0build\openjdk-8u232-b09
-call "%~dp0build\apache-ant-1.10.7\bin\ant" -buildfile "%~dp0build.xml" build %*
+call "%~dp0build\apache-ant-1.10.7\bin\ant" -buildfile "%~dp0build.xml" %* build
 if errorlevel 1 goto error
 call "%~dp0build\run.bat" %*
 if errorlevel 1 goto error
