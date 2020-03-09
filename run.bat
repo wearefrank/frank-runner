47247d8d1ee349f4d30ff96ffd1a29007e5dc2f0
Jaco de Groot <jaco.de.groot@ibissource.org>
2020-03-09 22:11:19 +0100

    Improve download and unzip robustness

--- run.bat
+++ run.bat
@@ -2,44 +2,36 @@
 if not exist "%~dp0download\" (
 	mkdir "%~dp0download"
 )
-if not exist "%~dp0build\" (
-	mkdir "%~dp0build"
+if not exist "%~dp0build\tmp\build\" (
+	mkdir "%~dp0build\tmp\build"
 )
 if not exist "%~dp0download\OpenJDK8U-jdk_x64_windows_8.zip" (
-	curl -o "%~dp0download\OpenJDK8U-jdk_x64_windows_8.zip" -L https://github.com/AdoptOpenJDK/openjdk8-upstream-binaries/releases/download/jdk8u232-b09/OpenJDK8U-jdk_x64_windows_8u232b09.zip
+	curl -o "%~dp0download\OpenJDK8U-jdk_x64_windows_8.zip.tmp" -L https://github.com/AdoptOpenJDK/openjdk8-upstream-binaries/releases/download/jdk8u232-b09/OpenJDK8U-jdk_x64_windows_8u232b09.zip
 	if errorlevel 1 (
-		if exist "%~dp0download\OpenJDK8U-jdk_x64_windows_8.zip" (
-			del "%~dp0download\OpenJDK8U-jdk_x64_windows_8.zip"
-		)
 		goto error
 	)
+	move "%~dp0download\OpenJDK8U-jdk_x64_windows_8.zip.tmp" "%~dp0download\OpenJDK8U-jdk_x64_windows_8.zip"
 )
 if not exist "%~dp0build\openjdk-8u232-b09\" (
-	tar xvf "%~dp0download\OpenJDK8U-jdk_x64_windows_8.zip" -C "%~dp0build"
+	tar xvf "%~dp0download\OpenJDK8U-jdk_x64_windows_8.zip" -C "%~dp0build\tmp\build"
 	if errorlevel 1 (
-		if exist "%~dp0build\openjdk-8u232-b09\" (
-			rmdir /S /Q "%~dp0build\openjdk-8u232-b09\"
-		)
 		goto error
 	)
+	move "%~dp0build\tmp\build\openjdk-8u232-b09" "%~dp0build\openjdk-8u232-b09"
 )
 if not exist "%~dp0download\apache-ant-1.10.7-bin.zip" (
-	curl -o "%~dp0download\apache-ant-1.10.7-bin.zip" -L http://apache.redkiwi.nl//ant/binaries/apache-ant-1.10.7-bin.zip
+	curl -o "%~dp0download\apache-ant-1.10.7-bin.zip.tmp" -L http://apache.redkiwi.nl//ant/binaries/apache-ant-1.10.7-bin.zip
 	if errorlevel 1 (
-		if exist "%~dp0download\apache-ant-1.10.7-bin.zip" (
-			del "%~dp0download\apache-ant-1.10.7-bin.zip"
-		)
 		goto error
 	)
+	move "%~dp0download\apache-ant-1.10.7-bin.zip.tmp" "%~dp0download\apache-ant-1.10.7-bin.zip"
 )
 if not exist "%~dp0build\apache-ant-1.10.7\" (
-	tar xvf "%~dp0download\apache-ant-1.10.7-bin.zip" -C "%~dp0build"
+	tar xvf "%~dp0download\apache-ant-1.10.7-bin.zip" -C "%~dp0build\tmp\build"
 	if errorlevel 1 (
-		if exist "%~dp0build\apache-ant-1.10.7\" (
-			rmdir /S /Q "%~dp0build\apache-ant-1.10.7\"
-		)
 		goto error
 	)
+	move "%~dp0build\tmp\build\apache-ant-1.10.7" "%~dp0build\apache-ant-1.10.7"
 )
 set JAVA_HOME=%~dp0build\openjdk-8u232-b09
 call "%~dp0build\apache-ant-1.10.7\bin\ant" -buildfile "%~dp0build.xml" %* build
