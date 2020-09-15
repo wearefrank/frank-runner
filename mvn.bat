0131bdb4825fa17524fb648dc548dd0845eafaf5
Jaco de Groot <jaco.de.groot@ibissource.org>
2020-09-15 12:14:24 +0200

    Make failonerror="true" work when a bat file is called from ant with the exec task

--- mvn.bat
+++ mvn.bat
@@ -49,4 +49,5 @@ if %errorlevel% equ 0 goto end
 rem https://superuser.com/questions/527898/how-to-pause-only-if-executing-in-a-new-window
 set arg0=%0
 if [%arg0:~2,1%]==[:] if not [%TERM_PROGRAM%] == [vscode] pause
+exit /b %errorlevel%
 :end
