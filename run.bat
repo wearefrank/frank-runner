2188081ce0be1b924ebfb7c86b54d6417c3f53bc
Jaco de Groot <jaco.de.groot@ibissource.org>
2021-07-01 01:11:19 +0200

    Add .sh files

--- run.bat
+++ run.bat
@@ -7,45 +7,45 @@ if not exist "%~dp0download\" (
 if not exist "%~dp0build\tmp\build\" (
 	mkdir "%~dp0build\tmp\build"
 )
-set download.help=download https://github.com/AdoptOpenJDK/openjdk8-upstream-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_windows_8u252b09.zip manually, move it to %~dp0download and restart this script
-if not exist "%~dp0download\OpenJDK8U-jdk_x64_windows_8u252b09.zip" (
-	echo In case of errors %download.help%
-	curl -f -o "%~dp0download\OpenJDK8U-jdk_x64_windows_8u252b09.zip.tmp" -L https://github.com/AdoptOpenJDK/openjdk8-upstream-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_windows_8u252b09.zip
+set DOWNLOAD_HELP=download https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_windows_hotspot_8u252b09.zip manually, move it to %~dp0download and restart this script
+if not exist "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u252b09.zip" (
+	echo In case of errors %DOWNLOAD_HELP%
+	curl -f -o "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u252b09.zip.tmp" -L https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_windows_hotspot_8u252b09.zip
 	if !errorlevel! neq 0 (
-		echo Please %download.help%
+		echo Please %DOWNLOAD_HELP%
 		goto error
 	)
-	move "%~dp0download\OpenJDK8U-jdk_x64_windows_8u252b09.zip.tmp" "%~dp0download\OpenJDK8U-jdk_x64_windows_8u252b09.zip"
+	move "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u252b09.zip.tmp" "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u252b09.zip"
 )
 if not exist "%~dp0build\openjdk-8u252-b09\" (
-	tar --exclude=*/demo --exclude=*/sample --exclude=*/manual --exclude=*/src.zip -xvf "%~dp0download\OpenJDK8U-jdk_x64_windows_8u252b09.zip" -C "%~dp0build\tmp\build"
+	tar --exclude=*/demo --exclude=*/sample --exclude=*/manual --exclude=*/src.zip -xvf "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u252b09.zip" -C "%~dp0build\tmp\build"
 	if !errorlevel! neq 0 (
-		echo Please %download.help%
+		echo Please %DOWNLOAD_HELP%
 		goto error
 	)
 	move "%~dp0build\tmp\build\openjdk-8u252-b09" "%~dp0build\openjdk-8u252-b09"
 )
-set download.help=download https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.8-bin.zip manually, move it to %~dp0download and restart this script
-if not exist "%~dp0download\apache-ant-1.10.8-bin.zip" (
-	echo In case of errors %download.help%
-	curl -f -o "%~dp0download\apache-ant-1.10.8-bin.zip.tmp" -L https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.8-bin.zip
+set DOWNLOAD_HELP=download https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.10-bin.tar.gz manually, move it to %~dp0download and restart this script
+if not exist "%~dp0download\apache-ant-1.10.10-bin.tar.gz" (
+	echo In case of errors %DOWNLOAD_HELP%
+	curl -f -o "%~dp0download\apache-ant-1.10.10-bin.tar.gz.tmp" -L https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.10-bin.tar.gz
 	if !errorlevel! neq 0 (
-		echo Please %download.help%
+		echo Please %DOWNLOAD_HELP%
 		goto error
 	)
-	move "%~dp0download\apache-ant-1.10.8-bin.zip.tmp" "%~dp0download\apache-ant-1.10.8-bin.zip"
+	move "%~dp0download\apache-ant-1.10.10-bin.tar.gz.tmp" "%~dp0download\apache-ant-1.10.10-bin.tar.gz"
 )
-if not exist "%~dp0build\apache-ant-1.10.8\" (
-	tar --exclude=*/demo --exclude=*/sample --exclude=*/manual --exclude=*/src.zip -xvf "%~dp0download\apache-ant-1.10.8-bin.zip" -C "%~dp0build\tmp\build"
+if not exist "%~dp0build\apache-ant-1.10.10\" (
+	tar --exclude=*/demo --exclude=*/sample --exclude=*/manual --exclude=*/src.zip -xvf "%~dp0download\apache-ant-1.10.10-bin.tar.gz" -C "%~dp0build\tmp\build"
 	if !errorlevel! neq 0 (
-		echo Please %download.help%
+		echo Please %DOWNLOAD_HELP%
 		goto error
 	)
-	move "%~dp0build\tmp\build\apache-ant-1.10.8" "%~dp0build\apache-ant-1.10.8"
+	move "%~dp0build\tmp\build\apache-ant-1.10.10" "%~dp0build\apache-ant-1.10.10"
 )
 set JAVA_HOME=%~dp0build\openjdk-8u252-b09
-set ANT_HOME=%~dp0build\apache-ant-1.10.8
-call "%~dp0build\apache-ant-1.10.8\bin\ant" -emacs -buildfile "%~dp0build.xml" %* run
+set ANT_HOME=%~dp0build\apache-ant-1.10.10
+call "%~dp0build\apache-ant-1.10.10\bin\ant" -emacs -buildfile "%~dp0build.xml" %* run
 if %errorlevel% equ 0 goto end
 :error
 rem https://superuser.com/questions/527898/how-to-pause-only-if-executing-in-a-new-window
