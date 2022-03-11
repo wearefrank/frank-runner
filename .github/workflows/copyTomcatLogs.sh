57087e35aa880ad22f038f4ba6e920e8f0d60437
Martijn Dirkse <martijn@integrationpartners.nl>
2022-03-11 17:44:48 +0100

    Fix bug

--- .github/workflows/copyTomcatLogs.sh
+++ .github/workflows/copyTomcatLogs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export TOMCAT_PATH=$(find ../.. -name apache-tomcat*)
-cp -R ${TOMCAT_PATH}/logs ../../tomcatLogs
+export TOMCAT_PATH=$(ls -d build/apache-tomcat*)
+cp -R ${TOMCAT_PATH}logs ../../tomcatLogs
