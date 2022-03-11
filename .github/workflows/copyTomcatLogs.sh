f5278654175e06120d8b3fa566610a733973998f
Martijn Dirkse <martijn@integrationpartners.nl>
2022-03-11 18:19:50 +0100

    Fix bugs

--- .github/workflows/copyTomcatLogs.sh
+++ .github/workflows/copyTomcatLogs.sh
@@ -3,4 +3,4 @@
 echo "Running in directory $(pwd)"
 export TOMCAT_PATH=$(find ../.. -name apache-tomcat* | grep -v "zip")
 echo "Apache Tomcat dir is ${TOMCAT_PATH}"
-cp -R ${TOMCAT_PATH}logs ../../tomcatLogs
+cp -R ${TOMCAT_PATH}/logs ../../tomcatLogs
