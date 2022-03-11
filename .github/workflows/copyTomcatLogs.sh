ab833c7cd0163dd76cd05b1ca6fdfc2bb9d8092a
Martijn Dirkse <martijn@integrationpartners.nl>
2022-03-11 18:01:03 +0100

    Fix bug

--- .github/workflows/copyTomcatLogs.sh
+++ .github/workflows/copyTomcatLogs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export TOMCAT_PATH=$(find . -name apache-tomcat* | grep -v "zip")
+export TOMCAT_PATH=$(find ../.. -name apache-tomcat* | grep -v "zip")
 cp -R ${TOMCAT_PATH}logs ../../tomcatLogs
