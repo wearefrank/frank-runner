477f3c16d3ad82cc8154c15e28fb89982e2050ae
Martijn Dirkse <martijn@integrationpartners.nl>
2022-03-11 18:08:42 +0100

    More info

--- .github/workflows/copyTomcatLogs.sh
+++ .github/workflows/copyTomcatLogs.sh
@@ -2,4 +2,5 @@
 
 echo "Running in directory $(pwd)"
 export TOMCAT_PATH=$(find ../.. -name apache-tomcat* | grep -v "zip")
+echo "Apache Tomcat dir is ${TOMCAT_PATH}"
 cp -R ${TOMCAT_PATH}logs ../../tomcatLogs
