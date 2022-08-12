468743cf890d0e32196de70fdfc134121261d5ea
Jaco de Groot <jaco.de.groot@ibissource.org>
2022-08-12 12:29:20 +0200

    Remove Contents/Home after unzip for MacOS

--- rerun.sh
+++ rerun.sh
@@ -4,16 +4,17 @@ FR_DIR="`pwd`/${BASH_SOURCE%/*}/"
 if [[ ! -d "${FR_DIR}download/" ]]; then
 	mkdir "${FR_DIR}download/"
 fi
-if [[ ! -d "${FR_DIR}build/tmp/" ]]; then
-	mkdir -p "${FR_DIR}build/tmp"
+if [[ ! -d "${FR_DIR}build/tmp/build" ]]; then
+	mkdir -p "${FR_DIR}build/tmp/build"
 fi
 ZIP=OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz
 URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz
 DIR=build/jdk8u292-b10
+SUB=
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	ZIP=OpenJDK8U-jdk_x64_mac_hotspot_8u292b10.tar.gz
 	URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_mac_hotspot_8u292b10.tar.gz
-	DIR=build/jdk8u292-b10/Contents/Home
+	SUB=Contents/Home
 fi
 DOWNLOAD_HELP="download ${URL} manually, move it to ${FR_DIR}download and restart this script"
 if [[ ! -f "${FR_DIR}download/${ZIP}" ]]; then
@@ -27,19 +28,18 @@ if [[ ! -f "${FR_DIR}download/${ZIP}" ]]; then
 	mv "${FR_DIR}download/${ZIP}.tmp" "${FR_DIR}/download/${ZIP}"
 fi
 if [[ ! -d "${FR_DIR}${DIR}/" ]]; then
-	tar --exclude=*/demo --exclude=*/sample --exclude=*/manual --exclude=*/src.zip -xzvf "${FR_DIR}download/${ZIP}" -C "${FR_DIR}build/tmp"
+	tar --exclude=*/demo --exclude=*/sample --exclude=*/manual --exclude=*/src.zip -xzvf "${FR_DIR}download/${ZIP}" -C "${FR_DIR}build/tmp/build"
 	retVal=$?
 	if [[ $retVal -ne 0 ]]; then
 		echo "Please ${DOWNLOAD_HELP}"
 		exit $retVal
 	fi
-	DIR_TO_MOVE=${DIR/build\/}
-	DIR_TO_MOVE=${DIR_TO_MOVE/\/*}
-	mv "${FR_DIR}build/tmp/${DIR_TO_MOVE}" "${FR_DIR}build/${DIR_TO_MOVE}"
+	mv "${FR_DIR}build/tmp/${DIR}//${SUB}" "${FR_DIR}${DIR}"
 fi
 ZIP=apache-ant-1.10.10-bin.tar.gz
 URL=https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.10-bin.tar.gz
 DIR=build/apache-ant-1.10.10
+SUB=
 DOWNLOAD_HELP="download ${URL} manually, move it to ${FR_DIR}download and restart this script"
 if [[ ! -f "${FR_DIR}download/${ZIP}" ]]; then
 	echo In case of errors ${DOWNLOAD_HELP}
@@ -52,20 +52,14 @@ if [[ ! -f "${FR_DIR}download/${ZIP}" ]]; then
 	mv "${FR_DIR}download/${ZIP}.tmp" "${FR_DIR}/download/${ZIP}"
 fi
 if [[ ! -d "${FR_DIR}${DIR}/" ]]; then
-	tar --exclude=*/demo --exclude=*/sample --exclude=*/manual --exclude=*/src.zip -xzvf "${FR_DIR}download/${ZIP}" -C "${FR_DIR}build/tmp"
+	tar --exclude=*/demo --exclude=*/sample --exclude=*/manual --exclude=*/src.zip -xzvf "${FR_DIR}download/${ZIP}" -C "${FR_DIR}build/tmp/build"
 	retVal=$?
 	if [[ $retVal -ne 0 ]]; then
 		echo "Please ${DOWNLOAD_HELP}"
 		exit $retVal
 	fi
-	DIR_TO_MOVE=${DIR/build\/}
-	DIR_TO_MOVE=${DIR_TO_MOVE/\/*}
-	mv "${FR_DIR}build/tmp/${DIR_TO_MOVE}" "${FR_DIR}build/${DIR_TO_MOVE}"
+	mv "${FR_DIR}build/tmp/${DIR}//${SUB}" "${FR_DIR}${DIR}"
 fi
-JDK_DIR=build/jdk8u292-b10
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	JDK_DIR=build/jdk8u292-b10/Contents/Home
-fi
-export JAVA_HOME="${FR_DIR}${JDK_DIR}"
+export JAVA_HOME="${FR_DIR}build/jdk8u292-b10"
 export ANT_HOME="${FR_DIR}build/apache-ant-1.10.10"
 "${FR_DIR}build/apache-ant-1.10.10/bin/ant" -emacs -buildfile "${FR_DIR}build.xml" "$@" rerun
