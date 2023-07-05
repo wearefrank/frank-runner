7f69e3aea58c3861f5655a95d735b2c196e62b38
Jaco de Groot <jaco.de.groot@ibissource.org>
2023-07-05 19:19:16 +0200

    Fix download was unexpected at this time

--- stop.bat
+++ stop.bat
@@ -9,10 +9,10 @@ if not exist "%~dp0build\tmp\build\" (
 )
 set DOWNLOAD_HELP=download https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip manually, move it to %~dp0download and restart this script
 if not exist "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip" (
-	echo In case of errors %DOWNLOAD_HELP%
+	echo "In case of errors %DOWNLOAD_HELP%"
 	curl -f -o "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip.tmp" -L https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip
 	if !errorlevel! neq 0 (
-		echo Please %DOWNLOAD_HELP%
+		echo "Please %DOWNLOAD_HELP%"
 		goto error
 	)
 	move "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip.tmp" "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip"
@@ -20,17 +20,17 @@ if not exist "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip" (
 if not exist "%~dp0build\jdk8u292-b10\" (
 	tar --exclude=*/demo --exclude=*/sample --exclude=*/manual --exclude=*/src.zip -xvf "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip" -C "%~dp0build\tmp\build"
 	if !errorlevel! neq 0 (
-		echo Please %DOWNLOAD_HELP%
+		echo "Please %DOWNLOAD_HELP%"
 		goto error
 	)
 	move "%~dp0build\tmp\build\jdk8u292-b10" "%~dp0build\jdk8u292-b10"
 )
 set DOWNLOAD_HELP=download https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.10-bin.zip manually, move it to %~dp0download and restart this script
 if not exist "%~dp0download\apache-ant-1.10.10-bin.zip" (
-	echo In case of errors %DOWNLOAD_HELP%
+	echo "In case of errors %DOWNLOAD_HELP%"
 	curl -f -o "%~dp0download\apache-ant-1.10.10-bin.zip.tmp" -L https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.10-bin.zip
 	if !errorlevel! neq 0 (
-		echo Please %DOWNLOAD_HELP%
+		echo "Please %DOWNLOAD_HELP%"
 		goto error
 	)
 	move "%~dp0download\apache-ant-1.10.10-bin.zip.tmp" "%~dp0download\apache-ant-1.10.10-bin.zip"
@@ -38,14 +38,14 @@ if not exist "%~dp0download\apache-ant-1.10.10-bin.zip" (
 if not exist "%~dp0build\apache-ant-1.10.10\" (
 	tar --exclude=*/demo --exclude=*/sample --exclude=*/manual --exclude=*/src.zip -xvf "%~dp0download\apache-ant-1.10.10-bin.zip" -C "%~dp0build\tmp\build"
 	if !errorlevel! neq 0 (
-		echo Please %DOWNLOAD_HELP%
+		echo "Please %DOWNLOAD_HELP%"
 		goto error
 	)
 	move "%~dp0build\tmp\build\apache-ant-1.10.10" "%~dp0build\apache-ant-1.10.10"
 )
-set JDK_8_DIR="%~dp0%build\jdk8u292-b10"
-set JDK_11_DIR="%~dp0%build\jdk-11.0.11+9"
-set JDK_17_DIR="%~dp0%build\jdk-17.0.5+8"
+set JDK_8_DIR=%~dp0%build\jdk8u292-b10
+set JDK_11_DIR=%~dp0%build\jdk-11.0.11+9
+set JDK_17_DIR=%~dp0%build\jdk-17.0.5+8
 set JAVA_HOME=%JDK_8_DIR%
 set ANT_HOME=%~dp0build\apache-ant-1.10.10
 call "%~dp0build\apache-ant-1.10.10\bin\ant" -Dfr.jdk.8.dir="%JDK_8_DIR%" -Dfr.jdk.11.dir="%JDK_11_DIR%" -Dfr.jdk.17.dir="%JDK_17_DIR%" -emacs -buildfile "%~dp0build.xml" %* stop
