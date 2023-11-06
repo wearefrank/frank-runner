9062500152329896829d9d0990b4e0ed9625853c
Jaco de Groot <jaco.de.groot@ibissource.org>
2023-11-06 10:16:34 +0100

    Fix bump jdk versions

--- start.bat
+++ start.bat
@@ -7,23 +7,23 @@ if not exist "%~dp0download\" (
 if not exist "%~dp0build\tmp\build\" (
 	mkdir "%~dp0build\tmp\build"
 )
-set DOWNLOAD_HELP=download https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip manually, move it to %~dp0download and restart this script
-if not exist "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip" (
+set DOWNLOAD_HELP=download https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u392-b08/OpenJDK8U-jdk_x64_windows_hotspot_8u392b08.zip manually, move it to %~dp0download and restart this script
+if not exist "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u392b08.zip" (
 	echo "In case of errors %DOWNLOAD_HELP%"
-	curl -f -o "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip.tmp" -L https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip
+	curl -f -o "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u392b08.zip.tmp" -L https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u392-b08/OpenJDK8U-jdk_x64_windows_hotspot_8u392b08.zip
 	if !errorlevel! neq 0 (
 		echo "Please %DOWNLOAD_HELP%"
 		goto error
 	)
-	move "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip.tmp" "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip"
+	move "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u392b08.zip.tmp" "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u392b08.zip"
 )
-if not exist "%~dp0build\jdk8u292-b10\" (
-	tar --exclude=*/demo --exclude=*/sample --exclude=*/manual --exclude=*/src.zip -xvf "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u292b10.zip" -C "%~dp0build\tmp\build"
+if not exist "%~dp0build\jdk8u392-b08\" (
+	tar --exclude=*/demo --exclude=*/sample --exclude=*/manual --exclude=*/src.zip -xvf "%~dp0download\OpenJDK8U-jdk_x64_windows_hotspot_8u392b08.zip" -C "%~dp0build\tmp\build"
 	if !errorlevel! neq 0 (
 		echo "Please %DOWNLOAD_HELP%"
 		goto error
 	)
-	move "%~dp0build\tmp\build\jdk8u292-b10" "%~dp0build\jdk8u292-b10"
+	move "%~dp0build\tmp\build\jdk8u392-b08" "%~dp0build\jdk8u392-b08"
 )
 set DOWNLOAD_HELP=download https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.10-bin.zip manually, move it to %~dp0download and restart this script
 if not exist "%~dp0download\apache-ant-1.10.10-bin.zip" (
@@ -43,9 +43,9 @@ if not exist "%~dp0build\apache-ant-1.10.10\" (
 	)
 	move "%~dp0build\tmp\build\apache-ant-1.10.10" "%~dp0build\apache-ant-1.10.10"
 )
-set JDK_8_DIR=%~dp0%build\jdk8u292-b10
-set JDK_11_DIR=%~dp0%build\jdk-11.0.11+9
-set JDK_17_DIR=%~dp0%build\jdk-17.0.5+8
+set JDK_8_DIR=%~dp0%build\jdk8u392-b08
+set JDK_11_DIR=%~dp0%build\jdk-11.0.21+9
+set JDK_17_DIR=%~dp0%build\jdk-17.0.8+7
 set JAVA_HOME=%JDK_8_DIR%
 set ANT_HOME=%~dp0build\apache-ant-1.10.10
 call "%~dp0build\apache-ant-1.10.10\bin\ant" -Dfr.jdk.8.dir="%JDK_8_DIR%" -Dfr.jdk.11.dir="%JDK_11_DIR%" -Dfr.jdk.17.dir="%JDK_17_DIR%" -emacs -buildfile "%~dp0build.xml" %* start
--- stop.sh
+++ stop.sh
@@ -7,13 +7,13 @@ fi
 if [[ ! -d "${FR_DIR}build/tmp/build" ]]; then
 	mkdir -p "${FR_DIR}build/tmp/build"
 fi
-ZIP=OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz
-URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz
-DIR=build/jdk8u292-b10
+ZIP=OpenJDK8U-jdk_x64_linux_hotspot_8u392b08.tar.gz
+URL=https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u392-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u392b08.tar.gz
+DIR=build/jdk8u392-b08
 SUB=
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	ZIP=OpenJDK8U-jdk_x64_mac_hotspot_8u292b10.tar.gz
-	URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_mac_hotspot_8u292b10.tar.gz
+	ZIP=OpenJDK8U-jdk_x64_mac_hotspot_8u392b08.tar.gz
+	URL=https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u392-b08/OpenJDK8U-jdk_x64_mac_hotspot_8u392b08.tar.gz
 	SUB=Contents/Home
 fi
 DOWNLOAD_HELP="download ${URL} manually, move it to ${FR_DIR}download and restart this script"
@@ -60,9 +60,9 @@ if [[ ! -d "${FR_DIR}${DIR}/" ]]; then
 	fi
 	mv "${FR_DIR}build/tmp/${DIR}//${SUB}" "${FR_DIR}${DIR}"
 fi
-JDK_8_DIR="${FR_DIR}build/jdk8u292-b10"
-JDK_11_DIR="${FR_DIR}build/jdk-11.0.11+9"
-JDK_17_DIR="${FR_DIR}build/jdk-17.0.5+8"
+JDK_8_DIR="${FR_DIR}build/jdk8u392-b08"
+JDK_11_DIR="${FR_DIR}build/jdk-11.0.21+9"
+JDK_17_DIR="${FR_DIR}build/jdk-17.0.8+7"
 export JAVA_HOME="${JDK_8_DIR}"
 export ANT_HOME="${FR_DIR}build/apache-ant-1.10.10"
 "${FR_DIR}build/apache-ant-1.10.10/bin/ant" -Dfr.jdk.8.dir="${JDK_8_DIR}" -Dfr.jdk.11.dir="${JDK_11_DIR}" -Dfr.jdk.17.dir="${JDK_17_DIR}" -emacs -buildfile "${FR_DIR}build.xml" "$@" stop
